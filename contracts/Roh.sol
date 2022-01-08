e50c0ae17ff104b7b410a8b909409394e3321f4c
Rohini <rohh3006@gmail.com>
2022-01-08 13:19:58 +0530

    3. delegated transfer

--- contracts/Roh.sol
+++ contracts/Roh.sol
@@ -7,7 +7,7 @@ contract Roh {
 
 
 mapping(address=>uint256) public balanceOf;
-
+mapping(address=>mapping(address=>uint256)) public allowance;
 uint256 public totalSupply;
 
 event Transfer(
@@ -15,13 +15,22 @@ event Transfer(
   address indexed _to,
   uint256 _value
   );
+
+  event Approval(
+
+   address indexed _owner,
+   address indexed _spender,
+   uint256 _value
+    );
 //name
+
+//we also need to build a trnasfer event
 
 string public name="Roh";
 string public symbol="Ro";
 string public standard="Roh v1.0";
 //symbol
-
+//we need to build an allowance
 
 
 constructor(uint256 _initialSupply) public {
@@ -49,5 +58,43 @@ emit Transfer(msg.sender,_to,_value);
   //return a booolean
   return true;
 }
+//delegate transfer--> one function will allow our account to approve the transfers, and other with handle the delegated transfer-one account to other without the sender mentioning
+
+  //we want an approve function
+
+//aaprove account spender to send value tokens
+  function approve(address _spender, uint256 _value) public returns(bool success){
+
+    //allowance
+    allowance[msg.sender][_spender]=_value;
+    //approve event
+    emit Approval(msg.sender,_spender,_value);
+    return true;
+  }
+  //we want a transfer from function this fn allows us to handle this delegated transfers
+
+  function transferFrom(address _from,address _to,uint256 _value) public returns(bool success){
+
+//add some requirements- that from has enough tokenInstance
+require(_value<=balanceOf[_from]);
+//require that the allowance is big enough
+require(_value<=allowance[_from][msg.sender]);
+
+//change the balanceOf
+balanceOf[_from]-=_value;
+balanceOf[_to]+=_value;
+//update the allowance
+allowance[_from][msg.sender]-=_value;
+//call the transfer event
+emit Transfer(_from,_to,_value);
+
+    //return a booolean
+    return true;
+  }
+
+
+
+
+
 
 }
